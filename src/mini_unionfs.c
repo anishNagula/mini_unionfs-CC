9a2c644c099b27ae0987a7ddd24481680f7929f6
Muskan Goenka <muskangoenka44@gmail.com>
2026-03-26 09:34:25 +0530

    feat: add basic FUSE initialization and argument parsing

--- src/mini_unionfs.c
+++ src/mini_unionfs.c
@@ -3,9 +3,27 @@
 #include <fuse3/fuse.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
+
+struct mini_unionfs_state {
+    char lower_dir[PATH_MAX];
+    char upper_dir[PATH_MAX];
+};
 
 int main(int argc, char *argv[])
 {
-    printf("Mini-UnionFS starting...\n");
-    return 0;
+    if (argc < 4) {
+        fprintf(stderr, "Usage: %s <lower> <upper> <mount>\n", argv[0]);
+        return 1;
+    }
+
+    struct mini_unionfs_state *state = malloc(sizeof(struct mini_unionfs_state));
+    if (!state) {
+        perror("malloc");
+        return 1;
+    }
+
+    printf("Lower: %s\nUpper: %s\nMount: %s\n", argv[1], argv[2], argv[3]);
+
+    return fuse_main(argc, argv, NULL, state);
 }
